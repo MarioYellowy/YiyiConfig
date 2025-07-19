8cffc94725ba670fab2955aa691195ca62f2a752
MarioYellowy <mario03eche@gmail.com>
2025-07-19 16:42:20 -0600

    pre grub config

--- modules/grub/grub-theme.nix
+++ modules/grub/grub-theme.nix
@@ -1,9 +1,16 @@
 { pkgs, ... }:
 
 let
-  grubTheme = ./Elegant-wave-blur-grub-themes/right-dark-1080p_1/Elegant-wave-blur-right-dark;
+  grubTheme = pkgs.stdenv.mkDerivation {
+    name = "elegant-grub-theme";
+    src = ./Elegant-wave-blur-grub-themes/right-dark-1080p_1/Elegant-wave-blur-right-dark;
+    installPhase = ''
+      mkdir -p $out
+      cp -r $src/* $out/
+    '';
+  };
 in {
-  environment.etc."grub/themes/Elegant-wave-blur-right-dark".source = grubTheme;
+  environment.etc."grub/themes/elegant".source = grubTheme;
 
   boot.loader = {
     systemd-boot.enable = false;
@@ -13,7 +20,7 @@ in {
       efiSupport            = true;
       efiInstallAsRemovable = true;
       device                = "nodev";
-      theme                 = "${grubTheme}/theme.txt";
+      theme                 = "/etc/grub/themes/elegant/theme.txt";
     };
 
     efi.canTouchEfiVariables = false;
