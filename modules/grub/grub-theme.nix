bfb3c3a7066029e5cbba6014f54bba95ad7cfee4
MarioYellowy <mario03eche@gmail.com>
2025-07-19 18:27:17 -0600

    pre grub config 4

--- modules/grub/grub-theme.nix
+++ modules/grub/grub-theme.nix
@@ -1,16 +1,14 @@
 { pkgs, ... }:
 
 let
-  grubTheme = pkgs.stdenv.mkDerivation {
-    name = "elegant-grub-theme";
+  grubTheme = pkgs.runCommand "grub-theme-elegant" {
     src = ./Elegant-wave-blur-grub-themes/right-dark-1080p_1/Elegant-wave-blur-right-dark;
-    installPhase = ''
-      mkdir -p $out
-      cp -r $src/* $out/
-    '';
-  };
+  } ''
+    mkdir -p $out
+    cp -r $src/* $out/
+  '';
 in {
-  environment.etc."grub/themes/Elegant-wave-blur-right-dark".source = ./Elegant-wave-blur-grub-themes/right-dark-1080p_1/Elegant-wave-blur-right-dark;
+  environment.etc."grub/themes/elegant".source = grubTheme;
 
   boot.loader = {
     systemd-boot.enable = false;
@@ -21,14 +19,8 @@ in {
       efiInstallAsRemovable = true;
       device = "nodev";
 
-      theme = "/boot/grub/themes/Elegant-wave-blur-right-dark/theme.txt";
-
-      extraPrepareConfig = ''
-        mkdir -p /boot/grub/themes/Elegant-wave-blur-right-dark
-        cp -r /etc/grub/themes/Elegant-wave-blur-right-dark/* /boot/grub/themes/Elegant-wave-blur-right-dark/
-  '';
+      theme = "/boot/grub/themes/elegant/theme.txt";
     };
-
     efi.canTouchEfiVariables = false;
   };
 }
