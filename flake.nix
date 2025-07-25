1ce4bd9903893b59f8153a2f8350075b3bf4e749
MarioYellowy <mario03eche@gmail.com>
2025-07-25 03:38:18 -0600

    configuration.nix remove

--- flake.nix
+++ flake.nix
@@ -1,41 +1,40 @@
 {
-  description = "Mi configuración de NixOS con flakes";
+  description = "Mi yiyi config de NixOS con flakes";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     hyprland.url = "github:hyprwm/Hyprland";
     flake-utils.url = "github:numtide/flake-utils";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, nixpkgs, unstable, hyprland, home-manager, flake-utils, ... }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { 
-      inherit system; 
+      inherit system;
       config.allowUnfree = true;
     };
     hmModule = home-manager.nixosModules.home-manager;
-    grubThemeModule = import ./modules/grub/grub-theme.nix { inherit pkgs; };
   in {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         inherit system;
+	pkgs = pkgs;
 	specialArgs = { inherit self inputs; };
         modules = [
 	  hmModule
-          ./configuration.nix
-	  {
-            programs.hyprland = {
-              enable = true;
-              package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-              portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-              xwayland.enable = true;
-	      withUWSM = true;
-            };
+	  ./modules.nix {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
           }
-	  grubThemeModule
+          {
+            system.stateVersion = "25.05";
+          }
 	];
       };
     };
---
+++ modules.nix
@@ -0,0 +1,112 @@
+{ config, pkgs, lib, inputs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./modules/hyprland/hyprland.nix
+    ./modules/waybar/waybar.nix
+    ./modules/grub/grub-theme.nix
+  ];
+
+  virtualisation.docker.enable = true;
+
+  networking = {
+    hostName = "nixos";
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 22 80 443 ];
+    };
+  };
+
+  i18n = {
+    defaultLocale = "es_MX.UTF-8";
+    supportedLocales = [ "en_US.UTF-8/UTF-8" "es_MX.UTF-8/UTF-8" ];
+  };
+
+  time.timeZone = "America/Mexico_City";
+
+  services = {
+    openssh.enable = true;
+
+    displayManager = {
+      sddm.enable = true;
+      sddm.wayland.enable = true;
+      defaultSession = "hyprland";
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    brave
+    nushell
+    discord
+    youtube-music
+    zed-editor
+    kitty
+    pavucontrol
+    networkmanagerapplet
+    neovim
+    yazi
+    git
+    prismlauncher
+    swaynotificationcenter
+    xcur2png
+    grim
+    gimp
+    slurp
+    swappy
+    surrealist
+    teams-for-linux
+    stow
+    starship
+    nwg-look
+    catppuccin-gtk
+    obs-studio
+    libreoffice
+    unzip
+    neofetch
+    dotnetCorePackages.sdk_9_0_3xx
+    htop
+    jetbrains.rider
+    jetbrains.idea-ultimate
+    kdePackages.qt6ct
+    kdePackages.qtmultimedia
+    gcc
+    tree
+    obsidian
+  ];
+
+  environment.etc."xdg/wayland-sessions/hyprland.desktop".text = ''
+    [Desktop Entry]
+    Name=Hyprland
+    Comment=Dynamic tiling Wayland compositor
+    Exec=Hyprland
+    Type=Application
+    DesktopNames=Hyprland
+  '';
+
+  fonts = {
+    fontDir.enable = true;
+    fontconfig.enable = true;
+    packages = with pkgs; [
+      nerd-fonts.fira-code
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.hack
+      font-awesome
+    ];
+  };
+
+  hardware.acpilight.enable = true;
+
+  programs.steam.enable = true;
+
+  users.users.mario = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "docker" ];
+    shell = pkgs.nushell;
+  };
+
+  home-manager.users.mario = import ./home.nix {
+    inherit config pkgs inputs;
+  };
+}
+
